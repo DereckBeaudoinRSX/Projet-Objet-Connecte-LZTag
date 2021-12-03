1337a3180bd78e6f781f9d7484647954faadd65f
SixFri <60512627+SixFri@users.noreply.github.com>
2021-12-03 10:40:13 -0500

    Update LaserTag.ino

--- LaserTag/LaserTag.ino
+++ LaserTag/LaserTag.ino
@@ -1,11 +1,52 @@
 const int buzzerPin = 12;
 const int buttonPin = 15;
+const int laserPin = 5;
+const int dataPin = 4;
+const int latchPin = 0;
+const int clockPin = 2;
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once: 
   pinMode(buzzerPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(laserPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  
   Serial.begin(9600);
+}
+
+void Fire(){
+    digitalWrite(laserPin, HIGH);
+    buzzer();
+    digitalWrite(laserPin, LOW);
+    delay(50);
+}
+
+
+void buzzer(){
+    tone(buzzerPin,2200); // then buzz by going high
+    tone(buzzerPin,1000);
+    tone(buzzerPin,500);
+    tone(buzzerPin,200);
+    tone(buzzerPin,500);
+    delayMicroseconds(10000);    // waiting
+    noTone(buzzerPin);  // going low
+    delayMicroseconds(10000);    // and waiting more
+    tone(buzzerPin,2200); 
+    tone(buzzerPin,1000);
+    delayMicroseconds(10000);    // waiting
+    noTone(buzzerPin);  // going low
+    delayMicroseconds(10000);    // and waiting more
+    tone(buzzerPin,100); 
+    delayMicroseconds(10000);    // waiting
+    noTone(buzzerPin);  // going low
+    delayMicroseconds(10000);    // and waiting more
+    tone(buzzerPin,100); 
+    delayMicroseconds(10000);    // waiting
+    noTone(buzzerPin);  // going low
+    delayMicroseconds(10000);    // and waiting more
 }
 
 void loop() {
@@ -13,15 +54,6 @@ void loop() {
   if(digitalRead(buttonPin) == HIGH){
     Serial.write("PRESSED");
     Serial.print('\n');
-    buzzer();
+    Fire();
   }
-}
-
-void buzzer(){
-  unsigned long time = millis();
-  while(millis() - time <= 100) {
-    int freq = random(100, 1000);
-    tone(buzzerPin, freq);
-  }
-  noTone(buzzerPin);
 }
